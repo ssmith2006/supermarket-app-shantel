b958f3f03b8cb3c9eb26fe5022d562f6fa9fc767
ssmith2006 <shantel.smith@hotmail.com>
2025-09-22 20:12:38 +0000

    project setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["workspaces/supermarket-app-shantel/frontend/index.html", "/workspaces/supermarket-app-shantel/frontend/src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
