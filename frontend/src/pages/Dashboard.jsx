4dd5d3eb00cca4eb119d052ce7717e29245b918a
ssmith2006 <shantel.smith@hotmail.com>
2025-09-28 14:59:29 -0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,18 @@
 export default function Dashboard() {
   const token = localStorage.getItem("token");
+  const username = "Customer";
+
+  if (!token) {
+    return (
+      <div className="p-6">
+      <h2 className="text-2xl font bold text-red-500">Please login</h2>
+      </div>
+    )
+  }
   return (
     <div className="p-6">
-      {" "}
-      <h2 className="text-2xl font-bold">Dashboard</h2>{" "}
-      {token ? <p>You are logged in </p> : <p>Please login.</p>}{" "}
+      <h2 className="text-2xl font-bold">Dashboard</h2>
+      {token ? <p>Welcome back, {username}! You are logged in!</p>}
     </div>
   );
 }
