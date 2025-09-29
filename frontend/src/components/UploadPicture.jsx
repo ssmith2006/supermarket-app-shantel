28189af1ff02c2327e59aa2cfac42af7aad31f35
ssmith2006 <shantel.smith@hotmail.com>
2025-09-29 19:38:14 +0000

    update and testing

--- frontend/src/components/UploadPicture.jsx
+++ frontend/src/components/UploadPicture.jsx
@@ -1,17 +1,20 @@
 import { useRef} from "react"
 
 function UploadPicture({id, refresh}){
-  const fileInputRef =useRef(null);
-  const handleFileChange = async(e) =>{
+  const fileInputRef = useRef(null);
+  const handleFileChange = async(e) => {
     const file = e.target.files[0];
 
     if (!file){
       alert("Please upload a file.")
       return;
     }
+    console.log("Selected file:", file.name);
+  alert(`Pretending to upload: ${file.name}`);
+  
       const formData = new FormData()
-      formData.append("image"/file);
-      const res = await fetch (`http://`, { //add codespace url
+      formData.append("image", file);
+      const res = await fetch (`https://miniature-parakeet-4jw4wxj4x44g377rj-3000.app.github.dev/inventory/${id}/upload-picture`, { //add codespace url
         method: "POST",
         body: formData,  
         headers: {
@@ -28,10 +31,10 @@ function UploadPicture({id, refresh}){
   }
   return (
     <div>
-      <button className="bg-yellow-500 text-white px-2 rounded" onClick{() => fileInputRef.current.click()}>Upload Picture</button>
+      <button className="bg-yellow-500 text-white px-2 py-2 rounded" onClick={() => fileInputRef.current.click()}>Upload Picture</button>
     <input type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange}></input>
     </div>
   )
 }
 
-export default UploadPicture
+export default UploadPicture;
