eeb475a90a9f2ae3bea8cb0eec9f00c1661f1944
ssmith2006 <shantel.smith@hotmail.com>
2025-09-29 19:39:07 +0000

    try/catch and styling

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,26 @@
 export default function Dashboard() {
   const token = localStorage.getItem("token");
-  const username = "Customer";
+  let user = null;
+  try {
+    user = JSON.parse(localStorage.getItem("user"))
+  } catch (error) {
+    user = null;
+  }
+
+  const name =user?.name || user?.username;
+  
 
   if (!token) {
     return (
       <div className="p-6">
-      <h2 className="text-2xl font bold text-red-500">Please login</h2>
+      <h2 className="text-2xl font-bold text-red-500">Please login</h2>
       </div>
-    )
+    );
   }
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold">Dashboard</h2>
-      {token ? <p>Welcome back, {username}! You are logged in!</p>}
+      <p className="text-3xl font-bold ">Welcome back, {name || "User"}! You are logged in!</p>
     </div>
   );
 }
