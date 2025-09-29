3299091fe939eb553a307509a5985738a7116985
ssmith2006 <shantel.smith@hotmail.com>
2025-09-29 19:33:47 +0000

    imported pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Inventory from "./pages/Inventory.jsx"
+import Customer from "./pages/Customer.jsx";
+import Sales from "./pages/Sales.jsx"
 
 export default function App() {
   return (
@@ -17,6 +19,8 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/inventory" element={<Inventory />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/sales" element={<Sales />} />
       </Routes>
     </>
   );
