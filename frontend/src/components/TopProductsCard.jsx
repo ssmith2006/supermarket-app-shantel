749e36ac38428ee4a08fc2d303eb5c34821c187f
ssmith2006 <shantel.smith@hotmail.com>
2025-09-30 22:40:03 +0000

    styling

--- frontend/src/components/TopProductsCard.jsx
+++ frontend/src/components/TopProductsCard.jsx
@@ -1,7 +1,7 @@
 function TopProductsCard({ products }) {
   return (
     <div className="bg-white shadow rounded p-4 mb-4">
-      <h2 className="text-xl font-semibold mt-5 mb-10">Top 5 Products by Unit</h2>
+      <h2 className="text-xl text-center font-semibold mt-5 mb-10">Top 5 Products by Unit</h2>
       <ol className="list-disc pl-5">
         {products.map((p) => (
           <li className="italic" key={p.product_id}>
