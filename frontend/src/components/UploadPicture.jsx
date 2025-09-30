e42ebd931f2f5d418202742ace40e8caf715b710
ssmith2006 <shantel.smith@hotmail.com>
2025-09-30 22:40:15 +0000

    update

--- frontend/src/components/UploadPicture.jsx
+++ frontend/src/components/UploadPicture.jsx
@@ -1,40 +1,59 @@
-import { useRef} from "react"
+import { useRef } from "react";
 
-function UploadPicture({id, refresh}){
+function UploadPicture({ id, refresh }) {
   const fileInputRef = useRef(null);
-  const handleFileChange = async(e) => {
+
+  const handleFileChange = async (e) => {
     const file = e.target.files[0];
 
-    if (!file){
-      alert("Please upload a file.")
+    if (!file) {
+      alert("Please upload a file.");
       return;
     }
     console.log("Selected file:", file.name);
-  alert(`Pretending to upload: ${file.name}`);
-  
-      const formData = new FormData()
-      formData.append("image", file);
-      const res = await fetch (`https://miniature-parakeet-4jw4wxj4x44g377rj-3000.app.github.dev/inventory/${id}/upload-picture`, { //add codespace url
-        method: "POST",
-        body: formData,  
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      });
+    
 
-    if(res.ok) {
-      alert("Image uploaded successfully!")
-    }else{
-      alert("An error has occurred.  Please try again!")
+    const formData = new FormData();
+    formData.append("image", file);
+    
+
+    const res = await fetch(
+      `https://miniature-parakeet-4jw4wxj4x44g377rj-3000.app.github.dev/inventory/${id}/upload-picture`,
+      {
+        //add codespace url
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (res.ok) {
+      alert("Image uploaded successfully!");
+      if (refresh) refresh();
+    } else {
+      alert("An error has occurred.  Please try again!");
     }
-      refresh()  
-  }
+  };
+
+
   return (
     <div>
-      <button className="bg-yellow-500 text-white px-2 py-2 rounded" onClick={() => fileInputRef.current.click()}>Upload Picture</button>
-    <input type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange}></input>
+      <button
+        className="bg-yellow-500 text-white px-2 py-2 rounded"
+        onClick={() => fileInputRef.current.click()}
+      >
+        Upload Picture
+      </button>
+      <input
+        type="file"
+        className="hidden"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
     </div>
-  )
+  );
 }
 
 export default UploadPicture;
